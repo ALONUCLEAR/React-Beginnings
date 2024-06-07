bb32ee33c539cb7c3716dcc93d95433c4f0b88d5
ALONUCLEAR <alonrazka58@gmail.com>
2024-06-07 22:08:31 +0300

    feat(minimax): make the minimax better by choosing faster paths to victory and longer paths to defeat

--- src/app/components/board/minimax.utils.ts
+++ src/app/components/board/minimax.utils.ts
@@ -7,6 +7,11 @@ type Action = {
   turn?: Turn;
 };
 
+type MinimaxResponse = {
+  value: number;
+  numberOfSteps: number;
+};
+
 const copyMatrix = <T>(board: T[][]): T[][] => [
   ...board.map((row) => [...row]),
 ];
@@ -16,15 +21,29 @@ export const getNextMove = (boardState: CellValue[][]): CellValue[][] => {
 
 const useMinimax = (board: CellValue[][]): CellValue[][] => {
   const actions = possibleActions(board, "O");
-  const options = actions.map((action) => ({
-    row: action.row,
-    col: action.col,
-    value: minimax(result(board, action), "X"),
-  }));
+  const options = actions.map((action, index) => {
+    const res = minimax(result(board, action), "X");
+
+    return {
+      row: action.row,
+      col: action.col,
+      value: res.value,
+      nuberOfSteps: res.numberOfSteps,
+      actionIndex: index,
+    };
+  });
 
   if (options.length > 0) {
     const lowestValue = Math.min(...options.map(({ value }) => value));
-    const actionIndex = options.findIndex(({ value }) => value === lowestValue);
+    const goodOptions = options.filter(({ value }) => value === lowestValue);
+    // if we can win, do it quickly, else: at least lose slowly
+    const desiredFunction = lowestValue < 0 ? Math.min : Math.max;
+    const desiredNumberOfSteps = desiredFunction(
+      ...goodOptions.map(({ nuberOfSteps }) => nuberOfSteps)
+    );
+    const actionIndex = goodOptions.find(
+      ({ nuberOfSteps }) => nuberOfSteps === desiredNumberOfSteps
+    )!.actionIndex;
     return result(board, actions[actionIndex]);
   }
 
@@ -61,21 +80,45 @@ function result(board: CellValue[][], action: Action): CellValue[][] {
 
   return res;
 }
-function minimax(board: CellValue[][], turn: Turn): number {
+function minimax(
+  board: CellValue[][],
+  turn: Turn,
+  numberOfSteps = 0
+): MinimaxResponse {
   const gameEndScore = evaluateGameEnd(board);
   if (gameEndScore) {
-    return gameEndScore;
+    return {
+      value: gameEndScore,
+      numberOfSteps,
+    };
   }
 
   const actions = possibleActions(board, turn);
   const nextTurn = swapTurns(turn);
-  const actionValues = actions.map((action) =>
-    minimax(result(board, action), nextTurn)
+  const actionResponses = actions.map((action) =>
+    minimax(result(board, action), nextTurn, numberOfSteps + 1)
   );
+  const desiredFunction = turn === "X" ? Math.max : Math.min;
 
+  const searchedValue = desiredFunction(
+    ...actionResponses.map(({ value }) => value)
+  );
+  const goodActions = actionResponses.filter(
+    ({ value }) => value === searchedValue
+  );
+  // if the opponent is bound to win, make it as slow as possible
+  // if the current player is bound to win, make it as quick as possible
+  let pathDesiredFunction = searchedValue < 0 ? Math.min : Math.max;
   if (turn === "X") {
-    return Math.max(...actionValues);
+    pathDesiredFunction = searchedValue > 0 ? Math.min : Math.max;
   }
 
-  return Math.min(...actionValues);
+  const bestGoodPathLength = Math.min(
+    ...goodActions.map(({ numberOfSteps }) => numberOfSteps)
+  );
+
+  return {
+    value: searchedValue,
+    numberOfSteps: bestGoodPathLength,
+  };
 }
